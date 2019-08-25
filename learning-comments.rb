1dc829f649ad6f7d8b1c7cf57d4cc40c33e39a58
Eddie Parker <eddie.parker88@gmail.com>
2019-08-25 00:43:24 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,21 @@
+# chance_of_rain = 1
+# puts "Let's go outside!"
+# if chance_of_rain > 0.5
+#  puts "Pack an umbrella!"
+# else
+#  puts "Enjoy the fine day!"
+# end
+# puts "Oh, and always wear sunscreen!"
+
+#chance_of_rain = 0.5 
+#if chance_of_rain <= 0.25 
+#  puts "Pack a sun shelter!"
+#elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+#  puts "Pack an umbrella!"
+#else
+#  puts "Stay home and read Hegel."
+#end 
+
+puts "You know what year it is?"
+this_year = Time.now.year
+puts "Hey, its 2019!" if this_year == 2019
